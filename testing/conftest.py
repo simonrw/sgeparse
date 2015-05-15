0f29c54ea69d5b7cc43ba1bfd56a06af34c1077c
Simon Walker <s.r.walker101@googlemail.com>
2015-05-15 14:25:35 +0100

    Add conftest with some globals

---
+++ testing/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+import os
+
+
+@pytest.fixture
+def root():
+    return os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+
+
+@pytest.fixture
+def data(root):
+    return os.path.join(root, 'testing', 'data', 'qjobs.xml')
