9f293aba87b22feb78ea60cab4930cace91d8d53
Simon Walker <s.r.walker101@googlemail.com>
2015-05-16 18:56:13 +0100

    Update setup script with better information

--- setup.py
+++ setup.py
@@ -2,9 +2,20 @@ from setuptools import setup, find_packages
 
 setup(name='sgeparse',
       author='Simon Walker',
+      author_email='s.r.walker101@googlemail.com',
       description='For parsing running SGE jobs',
       long_description=open('README.rst').read(),
-      author_email='s.r.walker101@googlemail.com',
+      url='https://pypi.python.org/pypi/sgeparse',
       version='0.0.1',
+      license='MIT',
       packages=find_packages('src'),
-      package_dir={'': 'src'},)
+      package_dir={'': 'src'},
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          ],
+      )
